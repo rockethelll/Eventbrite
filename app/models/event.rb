ba599f8db441e2c06651b1054bfd3b8cc46c9a5a
rockethelll <rockethell@hotmail.com>
2023-02-20 21:27:48 +0100

    corrections

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,10 +1,10 @@
 class Event < ApplicationRecord
   has_many :attendances
-  has_many :users through: :attendances
+  has_many :users, through: :attendances
   validates :start_date, presence: true, comparison: { greater_than: DateTime.now }
-  validates :duration, presence: true, numericality: { equal_to: %5 = 0 && > 0}
+  validates :duration, presence: true, numericality: { divisible_by: 5, greater_than: 0 }
   validates :title, presence: true, length: 5..140
   validates :description, presence: true, length: 20..10000
-  validates :price, presence: true, length: 1..10000
+  validates :price, presence: true, numericality: { greater_than: 0, less_than_or_equal_to: 10000 }
   validates :location, presence: true
 end

778c78095017bce77a9c2b12454bc3812301a350
rockethelll <rockethell@hotmail.com>
2023-02-21 12:01:26 +0100

    devise done

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
   root 'static_pages#index'
-  resources :contact
   devise_for :users
-  # get 'static_pages/secret'
+  get 'static_pages/secret'
 
   
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html

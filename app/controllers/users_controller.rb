af8456ab59ffec2deb753950015f0d48fea95e6d
rockethelll <rockethell@hotmail.com>
2023-02-22 00:24:17 +0100

    edit profil done

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,14 +3,24 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
-  def create
-  end
-
   def show
     @user = User.find(current_user.id)
     @event_ids = Attendance.where(user_id: 1).pluck(:event_id)
   end
 
   def update
+    @user = User.find(current_user.id)
+    @user.update(user_params)
+    redirect_to user_path(current_user.id)
+  end
+
+  def edit
+    @user = User.find(current_user.id)
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :description)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,9 @@
 Rails.application.routes.draw do
   root 'static_pages#index'
   devise_for :users
-  get 'static_pages/secret'
   resources :events, only: [:index, :new, :show, :create]
-  resources :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources :users, only: [:new, :show, :edit, :update]
+  get 'static_pages/secret'
+  
   mount LetterOpenerWeb::Engine, at: '/letter_opener' if Rails.env.development?
 end

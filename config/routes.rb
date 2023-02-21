6acb19bb391a144861ae351c1a8a875dc8423312
rockethelll <rockethell@hotmail.com>
2023-02-21 18:00:25 +0100

    event index ok

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   root 'static_pages#index'
   devise_for :users
   get 'static_pages/secret'
+  resources :events, only: [:index, :new, :show, :create]
 
-  
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,11 +6,26 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
+User.destroy_all
+Event.destroy_all
+
 10.times do
-  User.create!(first_name: Faker::Name.first_name,
-              last_name: Faker::Name.last_name,
-              email: Faker::Internet.email(domain: 'yopmail.com'),
-              description: Faker::Lorem.paragraphs,
-              password: 123456
-          )
+  User.create!(
+    first_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name,
+    email: Faker::Internet.email(domain: 'yopmail.com'),
+    description: Faker::Lorem.paragraph,
+    password: 123456
+  )
+end
+
+20.times do
+  Event.create!(
+    start_date: Faker::Date.forward(days: 23),
+    duration: 120,
+    title: Faker::Lorem.sentence,
+    description: Faker::Lorem.paragraph,
+    price: rand(5..50),
+    location: Faker::Address.city
+  )
 end

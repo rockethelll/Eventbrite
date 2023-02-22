35d715d59518ef051bb7c09f7c97d9609ca77ddf
rockethelll <rockethell@hotmail.com>
2023-02-22 15:09:53 +0100

    try sendgrid

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  after_create :welcome_send
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
@@ -9,6 +10,6 @@ class User < ApplicationRecord
   has_many :events, through: :attendances
 
   def welcome_send
-    # UserMailer.welcome_email(self).deliver_now
+    UserMailer.welcome_email(self).deliver_now
   end
 end

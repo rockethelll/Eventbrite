4f84df53876234bceab37abd4778b709ec23f402
rockethelll <rockethell@hotmail.com>
2023-02-22 15:45:33 +0100

    sendgrid with new mail

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,5 +1,5 @@
 class UserMailer < ApplicationMailer
-  default from: 'rockethell@hotmail.com'
+  default from: 'saeros@yopmail.com'
 
   def welcome_email(user)
     # on récupère l'instance user pour ensuite pouvoir la passer à la view en @user

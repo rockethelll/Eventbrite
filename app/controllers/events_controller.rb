551963b8a596e6dabaa93a9c5f88ebe090695ce0
rockethelll <rockethell@hotmail.com>
2023-02-22 18:06:55 +0100

    before modify view sign-up

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -4,6 +4,7 @@ class EventsController < ApplicationController
   end
 
   def new
+    @event = Event.new
   end
 
   def show
@@ -11,5 +12,19 @@ class EventsController < ApplicationController
   end
 
   def create
+    @event = Event.new(event_params)
+    if @event.save
+      flash[:notice] = 'Potin sauvegardé'
+      redirect_to event_path(@event.id)
+    else
+      flash[:alert] = 'Erreur : impossible de sauvegarder le potin'
+      render :new
+    end
+  end
+
+  private
+
+  def event_params
+    params.require(:event).permit(:start_date, :duration, :title, :description, :price, :location)
   end
 end
